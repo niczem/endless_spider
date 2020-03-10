b218764535745b1b3fe9464b00e1a73f87e1933c
niczem <nic@transparency-everywhere.com>
2020-03-10 06:44:45 +0100

    limit option for incoming link distribution set

--- dashboard/api.js
+++ dashboard/api.js
@@ -5,9 +5,28 @@ const fs = require("fs");
 
 let db = require('../db');
 
-function getIncommingLinkDistribution(){
+
+function isInt(data){
+	if(typeof data==='number' && (data%1)===0)
+		return true;
+	return false;
+}
+
+function getIncommingLinkDistribution(limit){
+	let limitString = '';
+
+	if(limit){
+		limit = parseInt(limit);
+		if(isInt(limit)){
+			limitString = `WHERE in_id<='${limit}' AND  out_id<='${limit}'`;
+		}else{
+			throw 'limit not integer'
+		}
+	}
+
 	return new Promise(function(resolve, reject){
-		db.connection.query("SELECT DISTINCT(out_id) as id, count(out_id) AS count FROM links GROUP BY out_id HAVING count > 2 ORDER by count DESC;", function (err, result, fields) {
+		console.log(`SELECT DISTINCT(out_id) as id, count(out_id) AS count FROM links ${limitString} GROUP BY out_id HAVING count > 2 ORDER by count DESC;`);
+		db.connection.query(`SELECT DISTINCT(out_id) as id, count(out_id) AS count FROM links ${limitString} GROUP BY out_id HAVING count > 2 ORDER by count DESC;`, function (err, result, fields) {
 				var distribution = {};
 				result.forEach(function(value,i){
 					if(typeof distribution[value.count] == 'undefined')
@@ -15,12 +34,16 @@ function getIncommingLinkDistribution(){
 					else
 						distribution[value.count]++;
 				});
-				console.log(distribution.length);
-				fs.writeFile("data/distribution.json", JSON.stringify(distribution), (err) => {
-				  if (err) reject(err);
-				  console.log("Successfully written to file.");
-				  resolve();
-				});
+				//if limit is given, do not overwrite file
+				if(limit){
+					  resolve(distribution);
+				}else{
+					fs.writeFile("data/distribution.json", JSON.stringify(distribution), (err) => {
+					  if (err) reject(err);
+					  console.log("Successfully written to file.");
+					  resolve(distribution);
+					});
+				}
 		});
 	});
 }
@@ -55,7 +78,7 @@ setInterval(function(){
 			getCrawledSites();
 		});
 	});
-}, 360000);
+}, 7200000);
 
 db.init();
 app.get('/', function(req, res) {
@@ -84,14 +107,26 @@ app.get('/api/getCrawledSites', function(req, res, next){
 		  		res.send(data);
 		});
 });
-app.get('/api/getIncommingLinkDistribution', function(req, res, next){
 
+//either loads incoming link distribution from file if no limit is defined (otherwise it will take ages with large samples)
+//see intervall function at beginning of file to see file creation
+//if limit is defined sql query is run
+app.get('/api/getIncommingLinkDistribution/:limit*?', function(req, res, next){
+
+	console.log('getting incomming link distribution');
+	if(req.params.limit){
+		console.log('with limit '+req.params.limit);
+		getIncommingLinkDistribution(req.params.limit).then(function(distribution){
+		  		res.send(distribution);
+		});
+	}else{
 		fs.readFile("data/distribution.json", "utf-8", (err, data) => {
 			if(err)
 				console.log(err);
 			else
 		  		res.send(data);
 		});
+	}
 });
 app.get('/api/getOutgoingLinks', function(req, res, next){
 		  db.connection.query("SELECT DISTINCT(in_id) as id, count(in_id) AS count FROM links GROUP BY in_id HAVING count > 1 ORDER by count DESC;", function (err, result, fields) {
@@ -108,4 +143,6 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/js', express.static(path.join(__dirname, 'js')));
 app.use('/icons', express.static(path.join(__dirname, 'icons')));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+let server = app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+server.setTimeout(720000);
